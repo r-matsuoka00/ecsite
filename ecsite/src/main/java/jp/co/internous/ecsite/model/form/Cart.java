faaa474bc46b188cadbb8bbd94286d4eebea13cf
r-matsuoka00 <r.matsuoka00@gmail.com>
2023-06-25 15:29:29 +0900

    Lesson_ecsite

---
+++ ecsite/src/main/java/jp/co/internous/ecsite/model/form/Cart.java
@@ -0,0 +1,34 @@
+package jp.co.internous.ecsite.model.form;
+import java.io.Serializable;
+public class Cart implements Serializable{
+	private int id;
+	private String goodsName;
+	private int price;
+	private int count;
+	
+	public void setId(int id) {
+		this.id=id;
+	}
+	public int getId() {
+		return id;
+	}
+	public String getGoodsName() {
+		return goodsName;
+	}
+	public void setGoodsName(String goodsName) {
+		this.goodsName = goodsName;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+}
